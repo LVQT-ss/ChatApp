6d774d36f4d4859fd15a305ebe7b55ad9db13224
LVQT <levietquocthinh@gmail.com>
2024-04-17 21:27:13 +0700

    fixed why css not working

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,3 +7,9 @@ var messageForm = document.querySelector('#messageForm');
 var messageInput = document.querySelector('#message');
 var messageArea = document.querySelector('#messageArea');
 var connectingElement = document.querySelector('.connecting');
+
+var stompClient:null = null ;
+var username: null = null;
+var colors: string[] = [
+
+]
