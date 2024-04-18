6288a08ac3f0306c15b5b7088ecd82ad5a9aa489
LVQT <levietquocthinh@gmail.com>
2024-04-18 22:49:33 +0700

    add onConnected for chat room

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -8,8 +8,44 @@ var messageInput = document.querySelector('#message');
 var messageArea = document.querySelector('#messageArea');
 var connectingElement = document.querySelector('.connecting');
 
-var stompClient:null = null ;
-var username: null = null;
-var colors: string[] = [
+var stompClient = null ;
+var username= null;
 
-]
+var colors = [
+    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
+
+
+
+
+function connect(event){
+    username = document.querySelector('#name').value.trim();
+    if (username){
+        usernamePage.classList.add('hidden');
+        chatPage.classList.remove('hidden');
+
+        var socket = new SocketJS();
+        stompClient = Stomp.over(socket);
+        stompClient.connect({},onConnected,onError);
+    }
+    event.preventDefault()
+}
+
+function onConnected(){
+//subsribe to the public topic
+    stompClient.subscribe('/topic/public',onMessageReceived);
+
+    //tell username to the server
+    stompClient.send('/app/chat.addUser',
+        {},
+        JSON.stringify({sender:username,type:'JOIN'})
+        )
+    connectingElement.classList.add('hidden');
+
+}
+function onMessageReceived(){
+
+}
+
+usernameForm.addEventListener('submit', connect, true);
